65aafc49e6abab723e4095ef99d82b0ed6c0ec6d
Ar G Ho <argho.roy488@gmail.com>
2024-07-09 12:05:18 +0600

    SplashScreen has been done.

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:crafty_bay_ecommerce_flutter/utility/path_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,8 +8,27 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('lib/assets/images/logo.svg'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: SvgPicture.asset(ImagePath().logo),
+            ),
+          ),
+          const CircularProgressIndicator(
+            semanticsLabel: 'Circular progress indicator',
+          ),
+          const SizedBox(
+            height: 7,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 16.0),
+            child: Text('Version 1.0'),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
       ),
     );
   }
--- lib/utility/path_util.dart
+++ lib/utility/path_util.dart
@@ -1,4 +1,4 @@
-class Path {
+class ImagePath {
   final String _basePath = 'lib/assets/images';
   String get logo => '$_basePath/logo.svg';
   String get logoNav => '$_basePath/logo_nav.svg';
