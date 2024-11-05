a491cf74af954987fe2d0683ffcda101ef99f2bc
Ar G Ho <argho.roy488@gmail.com>
2024-11-05 20:31:53 +0600

    Search Bar Added on HomeScreen

--- lib/presentation/UI/screens/home_screen.dart
+++ lib/presentation/UI/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:crafty_bay_ecommerce_flutter/presentation/UI/widget/circular_ico
 import 'package:crafty_bay_ecommerce_flutter/presentation/utility/path_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -25,15 +26,74 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: Icons.person_outlined,
               onTap: () {},
             ),
+            const SizedBox(width: 8),
             CircularIconButton(
               icon: Icons.phone_iphone_outlined,
               onTap: () {},
             ),
+            const SizedBox(width: 8),
             CircularIconButton(
               icon: Icons.notification_add_outlined,
               onTap: () {},
             ),
           ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Icon(
+                      Icons.search,
+                      color: Theme.of(context).primaryColor, // Corrected line
+                    ),
+                  ),
+                  hintText: 'Search',
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              CarouselSlider(
+                options: CarouselOptions(height: 150.0),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(color: Colors.amber),
+                          child: Text(
+                            'text $i',
+                            style: TextStyle(fontSize: 16.0),
+                          ));
+                    },
+                  );
+                }).toList(),
+              )
+            ],
+          ),
         ),
       ),
     );
