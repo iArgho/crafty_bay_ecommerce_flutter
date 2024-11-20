3aada8a2f5f9f7aed3983b45dabb8a8aa7dc476b
iArgho <argho.roy488@gmail.com>
2024-11-20 14:31:26 +0600

    Upate MainBottomNavScreen

--- lib/presentation/UI/screens/main_bottom_nav_screen.dart
+++ lib/presentation/UI/screens/main_bottom_nav_screen.dart
@@ -1,17 +1,98 @@
+import 'package:crafty_bay_ecommerce_flutter/presentation/utility/color_palette.dart';
 import 'package:flutter/material.dart';
 
 class MainBottomNavScreen extends StatefulWidget {
   const MainBottomNavScreen({super.key});
 
   @override
-  State<MainBottomNavScreen> createState() => _MainBottomNavScreenState();
+  State<MainBottomNavScreen> createState() =>
+      _BottomNavigationBarExampleState();
 }
 
-class _MainBottomNavScreenState extends State<MainBottomNavScreen> {
+class _BottomNavigationBarExampleState
+    extends State<MainBottomNavScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    BusinessScreen(),
+    SchoolScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: _widgetOptions[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColor.primaryColor,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Index 0: Home',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class BusinessScreen extends StatelessWidget {
+  const BusinessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Index 1: Business',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
+
+class SchoolScreen extends StatelessWidget {
+  const SchoolScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Index 2: School',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
