ae4bc0278a5339e9d703109c92caf3405b8427a4
iArgho <argho.roy488@gmail.com>
2024-11-24 17:04:02 +0600

    Product Details Slider has been added

--- lib/presentation/UI/screens/product_details_screen.dart
+++ lib/presentation/UI/screens/product_details_screen.dart
@@ -15,65 +15,71 @@ class ProductDetailsScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          CarouselSlider(
-           options: CarouselOptions(
-            height: 200.0,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 3),
-               onPageChanged: (int page, _) {
-              _selectedSlider.value = page;
-            },
-          ),
-            items: [1, 2, 3, 4, 5].map((i) {
-              return Builder(
-                builder: (BuildContext context) {
-                  return Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
-                        colors: [
-                          Color.fromARGB(255, 220, 220, 220),
-                          Colors.white,
-                        ],
-                        begin: Alignment.bottomCenter,
-                        end: Alignment.topCenter,
-                      ),
-                    ),
-                    width: MediaQuery.of(context).size.width,
-                    child: const Center(
-                      child: Icon(
-                        Icons.flutter_dash,
-                        size: 60.0,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  );
+          Stack(
+            children: [
+              CarouselSlider(
+               options: CarouselOptions(
+                height: 200.0,
+                autoPlay: true,
+                autoPlayInterval: const Duration(seconds: 3),
+                   onPageChanged: (int page, _) {
+                  _selectedSlider.value = page;
                 },
-              );
-            }).toList(),
+              ),
+                items: [1, 2, 3, 4, 5].map((i) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Color.fromARGB(255, 220, 220, 220),
+                              Colors.white,
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                          ),
+                        ),
+                        width: MediaQuery.of(context).size.width,
+                        child: const Center(
+                          child: Icon(
+                            Icons.flutter_dash,
+                            size: 120.0,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+               Padding(
+           padding: const EdgeInsets.only(top: 180.0),
+                 child: ValueListenableBuilder(
+                               valueListenable: _selectedSlider,
+                               builder: (context, value, _) {
+                  List<Widget> list = [];
+                  for (int i = 0; i < 5; i++) {
+                    list.add(
+                      Container(
+                        width: 12,
+                        height: 12,
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(6),
+                            color: value == i ? AppColor.primaryColor : Colors.white),
+                      ),
+                    );
+                  }
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: list,
+                  );
+                               },
+                             ),
+               ),
+            ],
           ),
-           ValueListenableBuilder(
-          valueListenable: _selectedSlider,
-          builder: (context, value, _) {
-            List<Widget> list = [];
-            for (int i = 0; i < 5; i++) {
-              list.add(
-                Container(
-                  width: 10,
-                  height: 10,
-                  margin: const EdgeInsets.symmetric(horizontal: 4),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      border: Border.all(color: Colors.grey),
-                      color: value == i ? AppColor.primaryColor : null),
-                ),
-              );
-            }
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: list,
-            );
-          },
-        ),
         ],
       ),
     );
