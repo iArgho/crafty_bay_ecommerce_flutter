0b1182f1c6caf38fc5a7949cfdff4f8c33b3fdf5
iArgho <argho.roy488@gmail.com>
2024-12-05 09:47:59 +0600

    CartScreen has been updated

--- lib/presentation/UI/screens/bottomnavscreen/bottom_cart_screen.dart
+++ lib/presentation/UI/screens/bottomnavscreen/bottom_cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:crafty_bay_ecommerce_flutter/presentation/UI/widget/cart_list_iteam_widget.dart';
+import 'package:crafty_bay_ecommerce_flutter/presentation/utility/color_palette.dart';
 import 'package:flutter/material.dart';
 
 class BottomCartScreen extends StatelessWidget {
@@ -17,6 +18,52 @@ class BottomCartScreen extends StatelessWidget {
           return const CartListItem();
         },
       ),
+      bottomNavigationBar: Container(
+              color: AppColor.primaryColor.withOpacity(0.1),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              height: 80,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Price'),
+                        Text(
+                          '\$100',
+                          style: TextStyle(
+                            color: AppColor.primaryColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        // Add your tap handling logic here
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: AppColor.primaryColor,
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text(
+                            'Add To Cart',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
